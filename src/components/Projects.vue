4c87a33c9a7f8bf0d2df74696d7c4a2a9bc984cf
tyhieshajohnson <johnsontyhiesha@gmail.com>
2024-01-17 15:30:35 +0200

    WIP

---
+++ src/components/Projects.vue
@@ -0,0 +1,20 @@
+<!-- HTML -->
+<template>
+  <div class="card-body col">
+    <img :src="project.image"/>
+    <h5 class="card-title">{{ project.heading }}</h5>
+    <p class="card-text">{{ project.text }}</p>
+    <a href="project.deploy" class="btn btn-primary">Netlify</a>
+    <a href="project.github" class="btn btn-primary">Github</a>
+  </div>
+</template>
+
+<!-- JavaScript -->
+<script>
+export default {
+  props: ["project"],
+};
+</script>
+
+<!-- CSS -->
+<style scoped></style>
