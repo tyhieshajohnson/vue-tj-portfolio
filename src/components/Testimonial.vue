d6b45999ef12e17e34c256a5de3036fa165e366c
Tyhiesha Johnson <johnsontyhiesha@gmail.com>
2024-04-17 09:47:10 +0200

    Resume View Mobile

--- src/components/Testimonial.vue
+++ src/components/Testimonial.vue
@@ -69,4 +69,53 @@ p {
   display: flex;
   justify-content: center;
 }
+
+@media screen and (max-width: 480px) {
+  .card-group {
+  width: 20rem;
+  height: 30rem;
+  padding: 20px;
+  gap: 10px;
+  margin-top: 35px;
+  box-shadow: 10px 10px 5px grey;
+  transform: translateY(0);
+  transition: transform 0.5s;
+}
+
+.card-group:hover {
+  transform: translateY(-20px);
+  box-shadow: 10px 10px 5px #ffdb58;
+}
+
+.row {
+  margin: 0 auto;
+}
+
+img {
+  width: 15rem;
+  height: 15rem;
+  justify-content: center;
+  display: flex;
+  margin: 0 auto;
+  opacity: 0.5;
+  filter: grayscale(100%);
+}
+
+img:hover {
+  opacity: 1;
+  transition: opacity 0.5s;
+  filter: none;
+}
+
+h5 {
+  margin-top: 5px;
+  display: flex;
+  justify-content: center;
+}
+
+p {
+  display: flex;
+  justify-content: center;
+}
+}
 </style>
